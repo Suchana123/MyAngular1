4cd74dfbe0bcab7470b56f48983a583b7340d292
Suchana123 <me.suchana.mitra@gmail.com>
2022-03-28 21:26:00 +0530

    component update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { MainNavigationComponent } from './library/main-navigation/main-navigation.component';
+import { RegistrationComponent } from './views/registration/registration.component';
+
+// MDB Modules
+ import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+// import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+// import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+// import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+// import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+// import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+// import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+// import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+// import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+// import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+// import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+// import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+// import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+// import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+// import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainNavigationComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
